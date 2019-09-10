b14733c1024ee0d1addb8fce9f39acc0e1da29dc
alexup71rus <alexup71rus@gmail.com>
2019-09-10 07:45:00 +0300

    1 file modified

--- components/Albums/Albums.js
+++ components/Albums/Albums.js
@@ -5,8 +5,8 @@ import './style.scss';
 export const Albums = ({albums}) => <ul>
   {
     albums.map(album => {
-      return <li>
-        <figure key={album.id} className='album-block'>
+      return <li key={album.id}>
+        <figure className='album-block'>
           <Link to={`/album${album.id}`} >
             <p><img src={album.cover} /></p>
             <figcaption>{album.name} ({album.count})</figcaption>
