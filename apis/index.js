62868739b3bee40855b92dea14b9437ca4ccb5d7
alexup71rus <alexup71rus@gmail.com>
2019-09-10 15:44:02 +0300

    2 files modified

--- apis/index.js
+++ apis/index.js
@@ -1,8 +1,7 @@
-// import { axios } from 'axios';
 import { dbVendor } from '../settings';
 
 /**
- * 
+ * Получить всех пользователей
  */
 export const getUsers = () => {
   return fetch(dbVendor + "alexup71rus/photosviewer-elfsight/users")
@@ -10,22 +9,27 @@ export const getUsers = () => {
 }
 
 /**
- * 
+ * Получить фотографии пользователя
+ * @param id пользователя
+ * @param дополнительные параметры фильтрации
  */
 export const getPhotos = (uid, query = {
   album_id: 0, limit: 0, page: 0
 }) => {
   return fetch(dbVendor + "alexup71rus/photosviewer-elfsight/photos?uid=" + uid + 
-  (query.album_id ? '&album_id=' + query.album_id : '') + 
-  (query.limit ? '&_limit=' + query.limit : '') + 
-  (query.page ? '&_page=' + query.page : ''))
+    (query.album_id ? '&album_id=' + query.album_id : '') + 
+    (query.limit ? '&_limit=' + query.limit : '') + 
+    (query.page ? '&_page=' + query.page : ''))
   .then(res => res.json());
 }
 
 /**
- * 
+ * Получить альбомы пользователя
+ * @param id пользователя
  */
 export const getAlbums = (id, uid) => {
-  return fetch(dbVendor + "alexup71rus/photosviewer-elfsight/albums?id="+ (id ? id : '') + (uid ? '&uid=' + uid : ''))
+  return fetch(dbVendor + "alexup71rus/photosviewer-elfsight/albums?id=" +
+    (id || '') +
+    (uid ? '&uid=' + uid : ''))
   .then(res => res.json());
 }
